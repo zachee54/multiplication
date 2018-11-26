746d85c712c58533985968af261578808028dfd5
Olivier HAAS <git.oshaas@dfgh.net>
2018-11-26 15:10:24 +0100

    Remplacement des guillemets par des apostrophes dans JavaScript

--- tables.js
+++ tables.js
@@ -3,12 +3,12 @@ var number1, number2;
 function createQuestion() {
 	number1 = createRandomNumber();
 	number2 = createRandomNumber();
-	setTextContent("text1", number1);
-	setTextContent("text2", number2);
+	setTextContent('text1', number1);
+	setTextContent('text2', number2);
 	
 	enableSendButton();
-	document.getElementById("answer").value = "";
-	document.getElementById("message").innerHTML = "";
+	document.getElementById('answer').value = '';
+	document.getElementById('message').innerHTML = '';
 }
 
 function createRandomNumber() {
@@ -21,22 +21,22 @@ function setTextContent(id, text) {
 }
 
 function checkAnswer() {
-	var answer = parseInt(document.getElementById("answer").value);
+	var answer = parseInt(document.getElementById('answer').value);
 	if (!isNaN(answer)) {
-		var messageNode = document.getElementById("message");
+		var messageNode = document.getElementById('message');
 		var solution = number1*number2;
-		messageNode.innerHTML = (answer == solution) ? "Bravo !" : ("Désolé !<br/>La bonne réponse était " + number1 + " x " + number2 + " = " + solution);
+		messageNode.innerHTML = (answer == solution) ? 'Bravo !' : ('Désolé !<br/>La bonne réponse était ' + number1 + ' x ' + number2 + ' = ' + solution);
 		
 		enableSolutionButton();
 	}
 }
 
 function enableSolutionButton() {
-	document.getElementById("send").disabled = true;
-	document.getElementById("nouveau").setAttribute("style", "display:block");
+	document.getElementById('send').disabled = true;
+	document.getElementById('nouveau').setAttribute('style', 'display:block');
 }
 
 function enableSendButton() {
-	document.getElementById("send").disabled = false;
-	document.getElementById("nouveau").setAttribute("style", "display:none");
+	document.getElementById('send').disabled = false;
+	document.getElementById('nouveau').setAttribute('style', 'display:none');
 }
