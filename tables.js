15c92049aecb9b9dadb288739a1fa6a65015861d
Olivier HAAS <git.oshaas@dfgh.net>
2018-11-26 15:26:26 +0100

    Utilisation de JQuery

--- tables.js
+++ tables.js
@@ -3,40 +3,34 @@ var number1, number2;
 function createQuestion() {
 	number1 = createRandomNumber();
 	number2 = createRandomNumber();
-	setTextContent('text1', number1);
-	setTextContent('text2', number2);
+	$('#text1').html(number1);
+	$('#text2').html(number2);
 	
 	enableSendButton();
-	document.getElementById('answer').value = '';
-	document.getElementById('message').innerHTML = '';
+	$('#answer').val('');
+	$('#message').html('');
 }
 
 function createRandomNumber() {
 	return Math.ceil(Math.random() * 10);
 }
 
-function setTextContent(id, text) {
-	var node = document.getElementById(id);
-	node.innerHTML = text;
-}
-
 function checkAnswer() {
-	var answer = parseInt(document.getElementById('answer').value);
+	var answer = parseInt($('#answer').val());
 	if (!isNaN(answer)) {
-		var messageNode = document.getElementById('message');
 		var solution = number1*number2;
-		messageNode.innerHTML = (answer == solution) ? 'Bravo !' : ('Désolé !<br/>La bonne réponse était ' + number1 + ' x ' + number2 + ' = ' + solution);
+		$('#message').html((answer == solution) ? 'Bravo !' : ('Désolé !<br/>La bonne réponse était ' + number1 + ' x ' + number2 + ' = ' + solution));
 		
 		enableSolutionButton();
 	}
 }
 
 function enableSolutionButton() {
-	document.getElementById('send').disabled = true;
-	document.getElementById('nouveau').setAttribute('style', 'display:block');
+	$('#send').prop('disabled', true);
+	$('#nouveau').show();
 }
 
 function enableSendButton() {
-	document.getElementById('send').disabled = false;
-	document.getElementById('nouveau').setAttribute('style', 'display:none');
+	$('#send').prop('disabled', false);
+	$('#nouveau').hide();
 }
